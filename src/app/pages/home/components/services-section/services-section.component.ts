fe3b46b523629ea38cbfa82815f8fe37b67fafe5
Moises Marchini <moisesmarchinipereira@gmail.com>
2023-10-10 15:48:02 -0300

    Add section services;

---
+++ src/app/pages/home/components/services-section/services-section.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { CardDefault } from '../../../../shared/components/card-default/card-default.component';
+
+@Component({
+  selector: 'app-services-section',
+  templateUrl: './services-section.component.html',
+  styleUrls: ['./services-section.component.scss']
+})
+export class ServicesSectionComponent {
+  cards: CardDefault[] = [
+    {
+      title: 'UIUX Design',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.'
+    },
+    {
+      title: 'Webflow devlopment',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.'
+    },
+    {
+      title: 'Marketing ',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.'
+    },
+    {
+      title: 'Branding',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.'
+    },
+  ]
+}
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 import { TrustedCompaniesComponent } from './components/trusted-companies/trusted-companies.component';
+import { ServicesSectionComponent } from './components/services-section/services-section.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   declarations: [
     HomeComponent,
     HeroSectionComponent,
-    TrustedCompaniesComponent
+    TrustedCompaniesComponent,
+    ServicesSectionComponent
   ]
 })
 export class HomeModule { }
